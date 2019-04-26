4325c0ced36f2e3c31613740fdbb9fe5fc948fd0
hachibu <raymondsohn@gmail.com>
2019-04-25 17:14:09 -0700

    Remove matching phrases
    Remove matching phrases because they lose their novelty quickly.

--- slack-lock-bot/bot.rb
+++ slack-lock-bot/bot.rb
@@ -20,30 +20,5 @@ module SlackLockBot
         desc 'List locks.'
       end
     end
-
-    MatchingPhrases = {
-      'adios' => 'adios',
-      ':wave:' => ':wave:',
-      ':success:' => ':success:',
-      ':parrot:' => ':parrot:',
-      'lunch' => 'enjoy your lunch',
-      'dinner' => 'enjoy your dinner',
-      'brb' => 'see you soon',
-      'chai time' => 'enjoy your chai',
-      '<< food' => 'enjoy your food',
-      '<< running' => 'have a great run',
-      '<< workout' => 'have a great workout',
-      'fyi' => 'thanks for letting me know'
-    }
-
-    MatchingPhrases.each do |phrase, reply|
-      match(Regexp.new(phrase, true)) do |client, data, match|
-        client.say(
-          channel: data.channel,
-          text: reply,
-          thread_ts: data.thread_ts || data.ts
-        )
-      end
-    end
   end
 end
--- spec/slack-lock-bot/bot_spec.rb
+++ spec/slack-lock-bot/bot_spec.rb
@@ -8,11 +8,4 @@ describe SlackLockBot::Bot do
   subject { app }
 
   it_behaves_like 'a slack ruby bot'
-
-  it 'responds to matching phrases' do
-    SlackLockBot::Bot::MatchingPhrases.each do |phrase, response_phrase|
-      message = "#{phrase} #{SlackRubyBot.config.user}"
-      expect(message: message).to respond_with_slack_message(response_phrase)
-    end
-  end
 end
